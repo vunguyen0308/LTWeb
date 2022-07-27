0d72dd8253acbdd1a4fcf3179f048915e565dcd4
leducminh74 <91834983+leducminh74@users.noreply.github.com>
2022-07-27 11:17:31 +0700

    update

---
+++ src/main/java/vn/hcmuaf/edu/fit/lab6/filter/OrderFilter.java
@@ -0,0 +1,30 @@
+package vn.hcmuaf.edu.fit.lab6.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(filterName = "OrderFilter",urlPatterns = "/order/*")
+public class OrderFilter implements Filter {
+    public void init(FilterConfig config) throws ServletException {
+    }
+
+    public void destroy() {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+        HttpSession session = httpServletRequest.getSession();
+        String referer = ((HttpServletRequest) request).getHeader("referer");
+        if(referer == null || !referer.endsWith("checkout")){
+            httpServletResponse.sendRedirect("checkout");
+        }else{
+            chain.doFilter(request,response);
+        }
+    }
+}
